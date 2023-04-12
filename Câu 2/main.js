5273cd69461e950bf64dad69ce8c5bc9cafc27d8
phanthehung263 <phanthehung263@gmail.com>
2023-04-12 23:58:22 +0700

    first commit

--- Câu 2/main.js
+++ Câu 2/main.js
@@ -1,23 +1,15 @@
 function numberOneTriangle() {
-    // Clear previous triangle if any
     let triangleDiv = document.querySelector('.triangle');
     triangleDiv.innerHTML = '';
-
-    // Get user input
     let num = parseInt(document.getElementById('number-input').value);
-
-    // Create rows of blocks
     for (let i = 1; i <= num; i++) {
       let rowDiv = document.createElement('div');
       rowDiv.classList.add('triangle-row');
-
-      // Create blocks for each row
       for (let j = 1; j <= i; j++) {
         let blockDiv = document.createElement('div');
         blockDiv.classList.add('triangle-block');
         rowDiv.appendChild(blockDiv);
       }
-
       triangleDiv.appendChild(rowDiv);
     }
   }
